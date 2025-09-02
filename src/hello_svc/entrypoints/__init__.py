e43adae550e20f5eefd9aa90eec2d68c2420f692
João Eurico "Bokomoko" Lima <joao.eurico@live.com>
2025-09-02 23:11:13 +0000

    moving files around

--- src/hello_svc/entrypoints/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-This module will have the entrypoints, that is, ways for data to
-be received and responded to by the service
-no processing, nor loops, nor iterative processing should occur here
-"""
--- src/hello_svc/entrypoints/asgi.py
+++
@@ -1,5 +0,0 @@
-from .views import app
-
-__all__ = [
-    "app"
-]  # only app will be exposed and thus only it could be imported elsewhere
